e196f65b7273f8013b3000586462e8bd38efc3a3
vishal <jakharvishalv@gmail.com>
2024-03-27 18:53:34 +0530

    jwt expiration fixed

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -6,15 +6,21 @@ export const verifyRequest = (req, res, next) => {
         const bearerToken = req.headers.authorization;
         if (bearerToken) {
             token = bearerToken.split(' ')[1];
-
         }
         if (!token) {
             return res.status(401).send("No token provided");
         }
 
-        const decoded = jwt.verify(token, process.env.INVOICE_SECRET);
-        res.locals.user = decoded;
-        next();
+        jwt.verify(token, process.env.INVOICE_SECRET, (err, decoded) => {
+            if (err) {
+                if (err.name === 'TokenExpiredError') {
+                    return res.status(401).send("Token expired");
+                }
+                return res.status(401).send("Invalid token");
+            }
+            res.locals.user = decoded;
+            next();
+        });
     } catch (error) {
         console.error(error);
         return res.status(401).send("Unauthorized Request");
